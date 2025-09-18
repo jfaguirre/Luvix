7fb74127bc41c9f16dc95ef499d72dd1e7ed5e90
Franc Aguirre <66891063+jfaguirre@users.noreply.github.com>
2025-09-17 23:01:47 -0600

    Se agrego el modelo UsuarioDTO a LuvixAdmin

---
+++ LuvixAdminServices/LuvixAdminServices/Models/DTOs/UsuarioDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuvixAdminServices.Models.DTOs
+{
+    internal class UsuarioDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? FotoPerfil { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string Estado { get; set; } = "activo";
+    }
+}
